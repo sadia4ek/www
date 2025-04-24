8bed35252f5b27d9e7db5eaef99dbae1b80ee81d
sadia4ek <kapybara1122@gmail.com>
2025-04-24 09:02:39 +0200

    Update cheaytest.lua

--- cheaytest.lua
+++ cheaytest.lua
@@ -1,7 +1,11 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
 local Window = Library.CreateLib("beta", "RJTheme3") 
+
 local Tab = Window:NewTab("qwe")
-local Section Tab:NewSection("Section Name")
+
+local Section = Tab:NewSection("Section Name")
+
 Section:NewButton("AutoStars", "ne rabotaet sosi", function()
     print("test")
 end
