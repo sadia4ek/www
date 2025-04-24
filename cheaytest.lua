3cfa73b89fa2e0271cb0d759a1a8db277f34bc78
sadia4ek <kapybara1122@gmail.com>
2025-04-24 12:06:56 +0200

    Update cheaytest.lua

--- cheaytest.lua
+++ cheaytest.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("Yeet A Friend (хелп)", "RJTheme3")
+local Window = Library.CreateLib("Yeet A Friend ура кефйщц", "RJTheme3")
 
 local Tab = Window:NewTab("Stars")
 local Section = Tab:NewSection("AutoFarm")
