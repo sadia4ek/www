797c7e490647d6c4c801d48ae8ac5210f66b94fa
sadia4ek <kapybara1122@gmail.com>
2025-04-24 08:49:39 +0200

    Update cheaytest.lua

--- cheaytest.lua
+++ cheaytest.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window Library.CreateLib("Name", "RJTheme5") 
+local Window Library.CreateLib("Name", "RJTheme3") 
 
 local Tab Window: NewTab("qwe") 
 
