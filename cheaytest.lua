0191766a7943b4308bbfe206906a508ea9f5a9a3
sadia4ek <kapybara1122@gmail.com>
2025-04-24 09:24:40 +0200

    Update cheaytest.lua

--- cheaytest.lua
+++ cheaytest.lua
@@ -7,16 +7,15 @@ local Tab = Window:NewTab("Stars")
 local Section = Tab:NewSection("TEST")
 
 Section:NewButton("Auto Farm Stars", "Teleport to stars", function()
-    local player = game.Players.LocalPlayer
-    local character = player.Character or player.CharacterAdded:Wait()
-    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-    
+    local lp = game.Players.LocalPlayer
+    local char = lp.Character or lp.CharacterAdded:Wait()
+    local hrp = char:WaitForChild("HumanoidRootPart")
     local starsFolder = workspace:WaitForChild("Stars")
-    
+
     for _, star in ipairs(starsFolder:GetChildren()) do
-    	if star:IsA("BasePart") then
-    		humanoidRootPart.CFrame = star.CFrame + Vector3.new(0, 5, 0) -- телепорт трохи вище
-    		wait(1) -- затримка між телепортами
-    	end
+        if star:IsA("BasePart") then
+            hrp.CFrame = star.CFrame + Vector3.new(0, 5, 0)
+            wait(0.5)
+        end
     end
 end)
