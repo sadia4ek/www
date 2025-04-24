3d6e042652e6ce116c064116cbc2b1b7406d6b72
sadia4ek <kapybara1122@gmail.com>
2025-04-24 11:21:09 +0200

    Update cheaytest.lua

--- cheaytest.lua
+++ cheaytest.lua
@@ -1,3 +1,4 @@
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
 local Window = Library.CreateLib("Yeet A Friend (0.1 тест)", "RJTheme3")
@@ -6,16 +7,7 @@ local Tab = Window:NewTab("Stars")
 
 local Section = Tab:NewSection("AutoFarm")
 
--- Початкова швидкість телепортації
-local teleportSpeed = 0.01
-
--- Слайдер для налаштування швидкості телепортації
-Section:NewSlider("Teleport Speed", "Adjust the speed of teleportation", 1, 0.01, function(s)
-    teleportSpeed = s  -- Оновлюємо швидкість телепортації
-    print("Teleport Speed: " .. teleportSpeed)  -- Виводимо нову швидкість в консоль
-end)
-
-Section:NewButton("AutoFarm Stars", "Teleport to stars (VIP STARS SKIP)", function()
+Section:NewButton("AutoFarm Stars", "Teleport to stars", function()
     local Players = game:GetService("Players")
     local lp = Players.LocalPlayer
 
@@ -35,8 +27,8 @@ Section:NewButton("AutoFarm Stars", "Teleport to stars (VIP STARS SKIP)", functi
 
             -- Телепортація
             local _, hrp = getCharacter()
-            hrp.CFrame = obj.CFrame + Vector3.new(0, 5, 0)
-            wait(teleportSpeed) -- Використовуємо слайдер для контролю затримки
+            hrp.CFrame = obj.CFrame + Vector3.new(0, 0, 0)
+            wait(0.1)-- Використовуємо слайдер для контролю затримки
         end
     end
 end)
