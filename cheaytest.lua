dbcdc183efcdb138c826a177420ecf9b5d9d8c50
sadia4ek <kapybara1122@gmail.com>
2025-05-06 08:00:36 +0300

    Update cheaytest.lua

--- cheaytest.lua
+++ cheaytest.lua
@@ -1,5 +1,4 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-
 local VirtualInputManager = game:GetService("VirtualInputManager")
 
 -- Все возможные клавиши (без пробела)
@@ -10,6 +9,9 @@ local possibleKeys = {
     Enum.KeyCode.P, Enum.KeyCode.Q, Enum.KeyCode.R, Enum.KeyCode.S, Enum.KeyCode.T,
     Enum.KeyCode.U, Enum.KeyCode.V, Enum.KeyCode.W, Enum.KeyCode.X, Enum.KeyCode.Y, Enum.KeyCode.Z,
 }
+
+-- Переменная для хранения задержки (по умолчанию 0.05)
+local pressDelay = 0.05
 
 -- Функция нажатия 18 случайных клавиш
 local function pressRandomKeys()
@@ -24,24 +26,38 @@ local function pressRandomKeys()
 
         -- Нажимаем и отпускаем
         VirtualInputManager:SendKeyEvent(true, key, false, game)
-        task.wait(0.05)
+        task.wait(pressDelay)
         VirtualInputManager:SendKeyEvent(false, key, false, game)
     end
 end
 
--- Создание GUI окна и кнопки
+-- Создание GUI окна
 local Window = Rayfield:CreateWindow({
-    Name = "Key Presser GUI",
-    LoadingTitle = "Загрузка...",
-    LoadingSubtitle = "Random Key Spammer",
+    Name = "Youtuber Simulator 2",
+    LoadingTitle = "Loading...",
+    LoadingSubtitle = "im in depression😛",
     ConfigurationSaving = {
         Enabled = false
     }
 })
 
-local Tab = Window:CreateTab("Главная", 4483362458)
+local Tab = Window:CreateTab("Edit", 4483362458)
+local Tab2 = Window:CreateTab("Video", 4483362458)
 
+-- Кнопка запуска
 Tab:CreateButton({
-    Name = "Нажать 18 случайных клавиш",
+    Name = "Fast Edit",
     Callback = pressRandomKeys,
+})
+
+-- Слайдер регулировки скорости
+Tab:CreateSlider({
+    Name = "Delay between keys",
+    Range = {0.01, 1.0},
+    Increment = 0.01,
+    Suffix = "s",
+    CurrentValue = pressDelay,
+    Callback = function(Value)
+        pressDelay = Value
+    end,
 })
